25c028a715787338b7325ecc622232e531ab53e7
Laura05010 <lcmmadrid5@gmail.com>
2020-08-25 13:53:22 -0400

    created main scrapping file

---
+++ demo_project/demo_project/spiders/dogs_breeds.py
@@ -0,0 +1,24 @@
+import scrapy
+
+class BreedSpider(scrapy.Spider):
+  name = 'dog_breed_scraper'
+  #List of URLS to scrape
+  start_urls = [http://www.dogbreedhealth.com/list-of-dog-breeds/]
+  
+  def parse(self, response):
+    for breed in response.xpath("//div[@class= 'entry-content']/ul/li"):
+
+      yield {
+        'alpha_headings': breed.xpath(.[@class ='alpha-heading']).extract_first()
+
+      }
+  
+  Alphabet = parse(self, response)
+  print(Alphabet)
+  
+#POTENTIAL STEPS TO TAKE
+    #iterate through A,B,C ..
+
+    #iterate through entire list
+
+

0557923504ac0dbe65aa17e7034a903d109cda94
Laura05010 <lcmmadrid5@gmail.com>
2020-08-25 14:11:39 -0400

    making the links as strings

--- demo_project/demo_project/spiders/dogs_breeds.py
+++ demo_project/demo_project/spiders/dogs_breeds.py
@@ -3,17 +3,17 @@ import scrapy
 class BreedSpider(scrapy.Spider):
   name = 'dog_breed_scraper'
   #List of URLS to scrape
-  start_urls = [http://www.dogbreedhealth.com/list-of-dog-breeds/]
+  start_urls = ['http://www.dogbreedhealth.com/list-of-dog-breeds/']
   
   def parse(self, response):
     for breed in response.xpath("//div[@class= 'entry-content']/ul/li"):
 
       yield {
-        'alpha_headings': breed.xpath(.[@class ='alpha-heading']).extract_first()
+        'alpha_headings': breed.xpath(".[@class ='alpha-heading']").extract_first()
 
       }
   
-  Alphabet = parse(self, response)
+  Alphabet = parse(response)
   print(Alphabet)
   
 #POTENTIAL STEPS TO TAKE
